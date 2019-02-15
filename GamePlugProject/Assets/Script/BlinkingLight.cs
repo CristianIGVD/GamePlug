7a45306d29708fcafb13106c3cc324bd8eb43b5f
Faisal <faisalsheikhyt@gmail.com>
2019-02-15 15:26:10 -0500

    Black Hole Scene Update
    Space scene update

---
+++ GamePlugProject/Assets/Script/BlinkingLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingLight : MonoBehaviour
+{
+    Light blinkLight;
+    public float minWaitTime;
+    public float MaxWaitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blinkLight = GetComponent<Light>();
+        StartCoroutine(flashing());
+    }
+
+    IEnumerator flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime,MaxWaitTime));
+            blinkLight.enabled = !blinkLight.enabled;
+        }
+    }
+}
